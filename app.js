c666fbc43d6f3e7e9d880a5f9b1fd83048573d6e
Hayat <rafiehayat5@gmail.com>
2023-04-20 12:37:51 +0530

    ..

--- app.js
+++ app.js
@@ -116,9 +116,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(3000);
   })
-  .then(() => console.log("DB Connection Successful"))
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
